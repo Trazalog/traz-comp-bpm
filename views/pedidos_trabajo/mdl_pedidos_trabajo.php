5ed4e8302cea698b5921f06a77dd2d1fb3ad0a55
Fernando <fleiva@trazalog.com>
2020-12-23 13:18:22 -0300

    Bug 22 TAREAS - No se puede agregar un pedido de trabajo size = 2 horas

--- views/pedidos_trabajo/mdl_pedidos_trabajo.php
+++ views/pedidos_trabajo/mdl_pedidos_trabajo.php
@@ -2,13 +2,14 @@
 <div class="modal modal-fade" id="mdl-peta">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-body">
+            <div class="xmodal-body">
                 <?php 
                    echo comp('frm-peta', base_url(BPM."Pedidotrabajo/index"), true);
                 ?>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn" data-dismiss="modal">Cancelar</button>
+                <button type="button" id="btn-accion" class="btn btn-primary btn-guardar" onclick="guardarTodo()">Guardar</button>
             </div>
         </div>
     </div>
