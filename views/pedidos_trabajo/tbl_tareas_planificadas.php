db038668a0da2f6e19f75e59f5703c0c2af872f6
Rogelio Sanchez <rnsanchez@trazalog.com>
2022-10-20 18:26:23 -0300

    Listo detalle de hitos con tareas planificadas
    -> Se desarrollo el apartado de "Tareas Planificadas" para el listado del pedido de trabajo, donde se encuentra un listado de los hitos creados y collapsados por hito, se encuentran sus respectivas tareas planificadas

--- views/pedidos_trabajo/tbl_tareas_planificadas.php
+++ views/pedidos_trabajo/tbl_tareas_planificadas.php
@@ -1,7 +1,13 @@
+<style>
+    #tareas_planificadas ul{
+        background-color: #e1e1e1;
+        border-radius: 35px;
+        border: 1px solid black;
+    }
+</style>
 <div class="row">
     <div class="col-xs 12 col-sm-12 col-md-12">
-        <h4>Hitos del pedido:</h4>
-        <div id="tareas_planificadas" title="Dentro del hito se encuentran las tareas planificadas">
+        <div id="tareas_planificadas" title="Click para ver detalle">
             <?php 
             if(!empty($listadoHitos)){
                 foreach ($listadoHitos as $key) {
@@ -12,36 +18,37 @@
                     </button>
                     <div class="collapse" id="<?php echo $key->hito_id ?>">
                         <div class="card card-body">
-                            <ul style="list-style-type: none;">
                             <?php 
                                 if(!empty($key->tareas->tarea)){
                                     foreach ($key->tareas->tarea as $tareas) {
                             ?>
-                                <li>Nombre: <b><?php echo $tareas->nombre ?></b></li>
-                                <li>Descripción: <b><?php echo $tareas->descripcion ?></b></li>
-                                <li>Estado: <b><?php echo $tareas->estado ?></b></li>
-                                <li>Asignada: <b><?php echo $tareas->user_id ?></b></li>
-                                <li>Fecha Asignación: <b><?php echo $tareas->fecha ?></b></li>
-                                <li>Fecha Fin: <b><?php echo $tareas->fec_fin ?></b></li>
-                                <li>Case: <b><?php echo $tareas->case_id ?></b></li>
-                                <li>Pedido Material: <b><?php echo $tareas->case_id ?></b></li>
-                                <li>FORMULARIO: <b><?php echo $tareas->case_id ?></b></li>
-                                <li>Duración: <b><?php echo $tareas->duracion ?></b></li>
-                                <hr>
+                            <ul>
+                                <div data-json='<?php echo json_encode($tareas) ?>'>
+                                    <li><u>Nombre</u>: <b><?php echo $tareas->nombre ?></b></li>
+                                    <li><u>Descripción</u>: <b><?php echo $tareas->descripcion ?></b></li>
+                                    <li><u>Estado</u>: <b><?php echo $tareas->estado ?></b></li>
+                                    <li><u>Asignada</u>: <b><?php echo $tareas->user_id ?></b></li>
+                                    <li><u>Fecha</u> Asignación: <b><?php echo $tareas->fecha ?></b></li>
+                                    <li><u>Fecha</u> Fin: <b><?php echo $tareas->fec_fin ?></b></li>
+                                    <li><u>Case</u>: <b><?php echo $tareas->case_id ?></b></li>
+                                    <li><u>Duración</u>: <b><?php echo $tareas->duracion ?></b></li>
+                                    <li><u>Pedido de Materiales</u>: <a class="btn btn-sm btn-link" title="Pedido de materiales" onclick="verDetallePedido(this);">Ver detalle</a></li>
+                                    <li><u>Formulario Asociado</u>: <a class="btn btn-sm btn-link" title="Formulario asociado a la tarea" onclick="showForm(this)">Ver detalle</a></li>
+                                </div>
+                            </ul>
                             <?php
                                     }
                                 }else{
-                                    echo "<h5>No se planificaron tareas para este hito.</h5>";
+                                    echo "<h5 class='centrar'>No se planificaron tareas para este hito.</h5>";
                                 }
                             ?>
-                            </ul>
                         </div>
                     </div>
                 </div>
             <?php
                 }
             }else{
-                echo "<h4>No se crearon hitos para este pedido de trabajo.</h4>";
+                echo "<h4 class='centrar'>No se crearon hitos para este pedido de trabajo.</h4>";
             }
             ?>
         </div>
